12a36af2be53795b5016d92c598af66111f5cbda
mao.miaoyan <mao.miaoyan@outlook.com>
2018-07-05 18:27:26 +0800

    video rotation enum and version to 0.1.13

--- QNRTCStreaming.Interop/RTCVideoRotation.h
+++ QNRTCStreaming.Interop/RTCVideoRotation.h
@@ -5,8 +5,10 @@ namespace QNRTCStreaming
 	{
 		public enum class RTCVideoRotation
 		{
-
+			kVideoRotation_0 = 0,
+			kVideoRotation_90 = 90,
+			kVideoRotation_180 = 180,
+			kVideoRotation_270 = 270
 		};
-
 	}
 }
